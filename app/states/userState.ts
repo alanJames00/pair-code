c911ee44953663d6b563b019e885c43e37f891c1
alanjames00 <alanjames6282@gmail.com>
2023-11-10 10:25:50 +0530

    add persistance for recoil states

--- app/states/userState.ts
+++ app/states/userState.ts
@@ -1,7 +1,10 @@
 import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
 
+const { persistAtom } = recoilPersist();
 
 export const userState = atom<string | null >({
     key:'user-state',
-    default: null
+    default: null,
+    effects_UNSTABLE: [persistAtom],
 });
