5249e9d673cc4222742136c833c390f5020a75d1
alanjames00 <alanjames6282@gmail.com>
2024-01-04 11:14:34 +0530

    complete code submit to rce and results

--- app/collab/space/[collabId]/SideBar.tsx
+++ app/collab/space/[collabId]/SideBar.tsx
@@ -17,39 +17,51 @@ import {
   } from "@/components/ui/dropdown-menu";
 import { codeLang } from "@/app/states/codeLang";
 import { CodeContent } from "@/app/states/codeContent";
+import { useState } from "react";
 
 export default function SideBar({ members }: { members: any[]}) {
 
+    // Recoil States
     const [output, setOutput] = useRecoilState(CodeResult);
     const [lang, setLang] = useRecoilState(codeLang);
-
     const [codeText] = useRecoilState(CodeContent);
     console.log(codeText);
 
+    // Component States
+    const[isRunning, setRunning] = useState(false);
 
     const langArray = [ {name:'javascript', val:'js'}, {name:'python',val:'py'}, {name:'c',val:'c'} , {name:'rust', val:'rs'}, {name:'java' ,val:'java'}]
+
+    // Required for piston-RCE
+    const langVers = [{val:'js' ,ver:'18.15.0'}, {val:'py',ver:'3.10.0'}, {val:'c', ver: '10.2.0'}, {val:'rs',ver:'1.68.2'}, {val:'java', ver:'15.0.2'}]
+
 
     async function handleCodeRun() {
        
         try {
 
+            // disable run btn
+            setRunning(true);
+
             const reqBody = {
     
-                    // language: runtime.val,
-                    // version: (langVers.find(opt => opt.val == runtime.val)).ver,
-                    // files: [
-                    //     {
-                    //         name: `sample_code.${runtime.val}`,
-                    //         content: codeText,
-                    //     }
-                    // ],
-                    // stdin: "",
-                    // args: ["1", "2", "3"],
-                    // compile_timeout: 10000,
-                    // run_timeout: 3000,
-                    // compile_memory_limit: -1,
-                    // run_memory_limit: -1
+                    language: lang.val,
+                    version: langVers.find((elem) => elem.val == lang.val)?.ver,
+                    files: [
+                        {
+                            name: `sample_code.${lang.val}`,
+                            content: codeText,
+                        }
+                    ],
+                    stdin: "",
+                    args: ["1", "2", "3"],
+                    compile_timeout: 10000,
+                    run_timeout: 3000,
+                    compile_memory_limit: -1,
+                    run_memory_limit: -1
                 }
+                console.log(reqBody);
+                
                 
             const resp = await fetch('https://emkc.org/api/v2/piston/execute', {method: 'POST', 
             headers: {
@@ -58,12 +70,29 @@ export default function SideBar({ members }: { members: any[]}) {
             
             const respJson = await resp.json();
             // handle code results
+            console.log(respJson);
+
+            if(respJson.run.code == 0) {
+                // successful exec
+                setOutput(respJson.run.stdout);
+            }
+            else if(respJson.run.code == 1) {
+                // error in code exec
+                setOutput(respJson.run.stderr.substring(0,300));
+            }
+            else if(respJson.run.signal == "SIGKILL") {
+                setOutput(`Time Limit Exceeded: SIGKILL \n ${respJson.run.stdout}`)
+            }   
+
+
         }
         catch(e) {
             console.log(e);
             
         }
 
+
+        setRunning(false);
     }
     
 
@@ -94,7 +123,10 @@ export default function SideBar({ members }: { members: any[]}) {
               </DropdownMenuContent>
             </DropdownMenu>
 
-            <Button className=" ml-4">Run</Button>
+            <Button 
+                disabled={isRunning}
+                onClick={handleCodeRun}
+                className=" ml-4">Run</Button>
             </div>
 
             <div className=" mb-4">
