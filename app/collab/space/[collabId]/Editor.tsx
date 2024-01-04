8131a23f930fd609ae353324f06093968f67e311
alanjames00 <alanjames6282@gmail.com>
2024-01-04 11:00:19 +0530

    add blue border for editor

--- app/collab/space/[collabId]/Editor.tsx
+++ app/collab/space/[collabId]/Editor.tsx
@@ -4,7 +4,7 @@ import Editor from '@monaco-editor/react';
 export default function CodeEditor({ value, onChange, lang } : { value:string, onChange: any, lang: string }) {
 
     return (
-    <div className='md:h-[90vh] md:w-[75%] h-[60vh]'>
+    <div className='md:h-[90vh] md:w-[75%] h-[60vh] md:border-blue-400 border-2'>
        <Editor
       language={lang}
       theme='vs-dark'
